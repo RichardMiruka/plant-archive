8eed1d4ba7f0cc0b38b929fb40da30d1f1f50ee0
StevenMo <stevenmolvin@gmail.com>
2023-06-27 22:32:57 +0300

    feat: Post and Get methods added.

--- app/app.py
+++ app/app.py
@@ -108,31 +108,39 @@ api.add_resource(UserID,'/users/<int:id>')
 #post-plant
 
 
+#post-plant
 class Plants(Resource):
-   def post(self):
-    new_plant = Plant(
-    user_id=request.form['user_id'],
-    plant_type = request.form['plant_type']
-    # updated_at = request.form['updated_at'],
-    # created_at =request.form['created_at']
-    )
-    db.session.add(new_plant)
-    db.session.commit()
-    response_dict = new_plant.to_dict()
-    response = make_response(
-    jsonify(response_dict),
-    201,
-    )
-    return response
-
-   def get(self):
-       plants=[plant.to_dict() for plant in Plant.query.all()]
-       return make_response(jsonify(plants),200)
-
+    def post(self):
+        new_plant=Plant(
+        plant_type = request.form['plant_type'],
+        user_id= request.form['user_id']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response=make_response (jsonify(new_plant.to_dict()), 201)
+        return response
+    def get(self):
+        plants=[plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants),200)
     
-
-
 api.add_resource(Plants, '/plants')
+
+class Plants_by_id(Resource):
+    def get(self, id=None):
+        if id:
+            plant = Plant.query.filter_by(id=id).first().to_dict()
+            if plant:
+                return make_response(jsonify(plant), 200)
+        else:
+            response_dict={
+                "message": 'Plant not found'
+                }   
+            abort = make_response(
+                jsonify(response_dict), 404
+                )
+            return abort
+
+api.add_resource(Plants_by_id, '/plants/<int:id>')
 
 
 

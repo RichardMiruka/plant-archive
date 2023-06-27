0c3db25208892381dcc00de3648a4c4e8c6caca9
beatrice <helgapaul389@gmail.com>
2023-06-27 21:36:07 +0300

    added get method for plants

--- app/app.py
+++ app/app.py
@@ -106,13 +106,33 @@ api.add_resource(UserID,'/users/<int:id>')
 
 
 #post-plant
+
+
 class Plants(Resource):
-    def post(self):
-        pass
+   def post(self):
+    new_plant = Plant(
+    user_id=request.form['user_id'],
+    plant_type = request.form['plant_type']
+    # updated_at = request.form['updated_at'],
+    # created_at =request.form['created_at']
+    )
+    db.session.add(new_plant)
+    db.session.commit()
+    response_dict = new_plant.to_dict()
+    response = make_response(
+    jsonify(response_dict),
+    201,
+    )
+    return response
+
+   def get(self):
+       plants=[plant.to_dict() for plant in Plant.query.all()]
+       return make_response(jsonify(plants),200)
+
+    
 
 
-
-#api.add_resource(Plants, '/plants')
+api.add_resource(Plants, '/plants')
 
 
 

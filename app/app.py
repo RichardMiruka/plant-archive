72b72bb09cf0fe7202b334a916ea781009f18ea6
Job koech <jobkoech610@gmail.com>
2023-06-27 15:59:57 +0300

    patch and delete user

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ api =Api(app)
 db.init_app(app)
 
 migrate = Migrate(app, db)
+#index
 class Index (Resource):
    def get(self):
        response_dict={
@@ -31,26 +32,140 @@ class Index (Resource):
        return response
 api.add_resource(Index,'/')  
 
-class User (Resource):
+
+
+#post-user
+class Users(Resource):
     def post(self):
         new_user = User(
-            name = request.form['name'],
-            updated_at = request.form['updated_at'],
-            created_at =request.form['created_at'])
-        
+            name = request.form['name']
+            # updated_at = request.form['updated_at'],
+            # created_at =request.form['created_at']
+            )
         db.session.add(new_user)
         db.session.commit()
         response_dict = new_user.to_dict()
         response = make_response(
-            response_dict,
+            jsonify(response_dict),
             201,
         )
         return response
-api.add_resource(User, '/User')
+api.add_resource(Users, '/users')
+
+#delete-user and patch-user
+class UserID(Resource):
+    def delete(self, id):
+        user=User.query.get(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return "", 204
+        else:
+            response_dict={
+                "message": 'no user'
+            }
+            response = make_response(
+                jsonify(response_dict), 404
+            )
+            return response
+
+    def patch(self, id):
+        user = User.query.get(id)
+        
+
+        if user:
+                for attr in request.form:
+                  setattr(user,attr,request.form.get(attr))
+                db.session.add(user)
+                db.session.commit()
+
+                user_dict= user.to_dict()
+
+                response = make_response(
+                    jsonify(user_dict), 200
+                )
+
+                return response
+        else:
+                response_dict={
+                "message": 'no user'
+               }
+                response = make_response(
+                jsonify(response_dict), 404
+               )
+                return response
+
+api.add_resource(UserID,'/users/<int:id>')    
 
 
-        
-        
-        
+
+
+
+
+
+
+#post-plant
+class Plants(Resource):
+    def post(self):
+        pass
+
+
+
+#api.add_resource(Plants, '/plants')
+
+
+
+
+
+
+
+
+#delete-plant and patch-plant
+
+
+
+    
 if __name__ =='_main_':
-   app.run(port=5555)
+   app.run(port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -5,20 +5,24 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 class User(db.Model,SerializerMixin):
     __tablename__='users'
+    serialize_rules=('-plants.user',)
     id= db.Column('id',db.Integer(),primary_key=True)
     name = db.Column("name",db.String())
-    serialize_rules=('-plant.user')
+    
     plants=db.relationship('Plant', backref= 'user')
     
     created_at = db.Column('created_at', db.DateTime, default=db.func.now())
     updated_at = db.Column('updated_at', db.DateTime, onupdate=db.func.now())
     
-class Plant(db.Model, SerializerMixin):
+    def __repr__(self):
+        return f'<User {self.name}.>'
+class Plant(db.Model,SerializerMixin):
     __tablename__="plants"
+    serialize_rules=('-user.plants',)
     id = db.Column('id',db.Integer(),primary_key=True)
     user_id = db.Column('user_id',db.Integer(),db.ForeignKey('users.id'))
     plant_type = db.Column('plant_type', db.String())
-    serialize_rules=('-user.plants')
+   
     
     created_at = db.Column('created_at', db.DateTime, default=db.func.now())
     updated_at = db.Column('updated_at', db.DateTime, onupdate=db.func.now())
